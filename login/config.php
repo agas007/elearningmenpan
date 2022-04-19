743d9321b65d84ba57791e96607c815d974da38c
agas007 <agas9403@gmail.com>
2022-04-19 10:57:09 +0700

    change config for connecting to database

--- login/config.php
+++ login/config.php
@@ -1,9 +1,9 @@
 <?php 
  
-$server = "localhost";
-$user = "root";
-$pass = "";
-$database = "elearning";
+$server = "127.0.0.1:3306";
+$user = "u748553090_agastya";
+$pass = "BIROsdmu!2020";
+$database = "u748553090_elearning";
  
 $conn = mysqli_connect($server, $user, $pass, $database);
  
