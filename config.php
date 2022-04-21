72ca72d0728e95c686e511d57f2770435a6c383a
agas007 <agas9403@gmail.com>
2022-04-21 23:08:40 +0700

    add config.php for connecting database

---
+++ config.php
@@ -0,0 +1,20 @@
+<?php 
+ 
+$server = "127.0.0.1:3306";
+$user = "u748553090_agastya";
+$pass = "BIROsdmu!2020";
+$database = "u748553090_elearning";
+
+// database local
+// $server = "localhost";
+// $user = "root";
+// $pass = "";
+// $database = "elearning";
+ 
+$conn = mysqli_connect($server, $user, $pass, $database);
+ 
+if (!$conn) {
+    die("<script>alert('Gagal tersambung dengan database.')</script>");
+}
+ 
+?>
