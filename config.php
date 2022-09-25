866b0f2eaba44017ceaee576d14b91ff8ac33773
agas007 <agas9403@gmail.com>
2022-09-25 13:46:22 +0700

    database configuration fix

--- config.php
+++ config.php
@@ -1,16 +1,16 @@
 <?php 
 
 // database web
-// $server = "127.0.0.1:3306";
-// $user = "u748553090_agastya";
-// $pass = "BIROsdmu!2020";
-// $database = "u748553090_elearning";
+$server = "127.0.0.1:3306";
+$user = "u748553090_agastya";
+$pass = "BIROsdmu!2020";
+$database = "u748553090_elearning";
 
 // database local
-$server = "localhost";
-$user = "root";
-$pass = "";
-$database = "elearning";
+// $server = "localhost";
+// $user = "root";
+// $pass = "";
+// $database = "elearning";
  
 $conn = mysqli_connect($server, $user, $pass, $database);
  
